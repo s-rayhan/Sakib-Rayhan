c6075f53ac6b1cde676ac6c5a3a547126c7d8d7f
s-rayhan <101197983+s-rayhan@users.noreply.github.com>
2023-04-07 16:30:05 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+const form = document.getElementById('post-form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault(); // prevent default form submission
+
+  // get form data
+  const formData = new FormData(event.target);
+  const title = formData.get('title');
+  const author = formData.get('author');
+  const content = formData.get('content');
+
+  // create post object
+  const post = {
+    title: title,
+    author: author,
+    content: content
+  };
+
+  // save post object to local storage
+  localStorage.setItem('post', JSON.stringify(post));
+
+  // redirect to post page
+  window.location.href = 'post.html';
+});
